88944369101187248ec009be51f0f5b5bda5a904
asyncfinkd <shamgoingzuck@gmail.com>
2021-09-13 00:34:17 +0400

    Careers Schema, Start Backend

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const mongoose = require("mongoose");
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+process.env.PWD = process.cwd();
+
+mongoose.connect(
+  "mongodb+srv://bogcareers:nikanikonika@cluster0.qet1t.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use(cors());
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, () => {
+  console.log(`Server listen ${PORT} port`);
+});
---
+++ server/schema/job/JobSchema.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const JobFunctions = mongoose.Schema({
+  title: {
+    type: String,
+  },
+});
+
+const WorkExperiences = mongoose.Schema({
+  title: {
+    type: String,
+  },
+});
+
+const knows = mongoose.Schema({
+  title: {
+    type: String,
+  },
+});
+
+const JobDescription = mongoose.Schema({
+  title: {
+    type: String,
+  },
+  functions: [JobFunctions],
+  qualitificationRequire: {
+    type: String,
+  },
+  workExperience: [WorkExperiences],
+  know: [knows],
+});
+
+const JobSchema = mongoose.Schema({
+  title: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+  description: [JobDescription],
+  lastTime: {
+    type: String,
+  },
+});
