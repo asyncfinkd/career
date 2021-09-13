5087676d22b22244b25c02b68a0f8c9a66c2b04a
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-09-13 12:49:38 +0400

    Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Banner from "./components/banner/Banner";
-import Footer from "./components/footer/Footer";
-import Form from "./components/form/Form";
-import Navbar from "./components/navbar/Navbar";
-import Notification from "./components/notification/Notification";
 import axios from "axios";
 import env from "./application/environment/env.json";
 import { ApplicationContext } from "./application/context/ApplicationContext";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import IndexPages from "./pages/index/indexPages";
 
 export default function App() {
   const [jobs, setJobs] = React.useState([]);
@@ -16,111 +13,11 @@ export default function App() {
   return (
     <>
       <ApplicationContext.Provider value={{ jobs }}>
-        <Navbar />
-        <section className="flex-1 ie-no-flex1">
-          <Banner />
-          <div
-            id="jobs"
-            className="bg-gray-200 border-t-2 p-4 md:py-10 md:px-0 hh-job-listings"
-          >
-            <div className="fade-in animate-slow max-w-screen-md mx-auto">
-              <div className="bg-white p-4 rounded shadow-md">
-                <Form />
-              </div>
-
-              {jobs.map((item) => {
-                return (
-                  <>
-                    <div class="hh-jobs-openings">
-                      <a
-                        href="/job/83543/-"
-                        class="bg-white border-1 border-gray-400 p-4 block mt-3 first:mt-0 rounded hh-job-row transition-shadow duration-200 shadow-none hover:shadow-md"
-                      >
-                        <h3 class="font-semibold text-xl leading-8 hh-job-row-title">
-                          <span class="user-color text-secondary">
-                            {item.title}
-                          </span>
-                        </h3>
-                        <div class="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap">
-                          <div class="mt-2 flex items-center text-sm leading-5 text-gray-600 sm:mr-6 hh-job-row-location">
-                            <inline-svg>
-                              <svg
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                class="flex-shrink-0 mr-1 h-5 w-5 text-gray-500"
-                              >
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                                ></path>
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                                ></path>
-                              </svg>
-                            </inline-svg>
-                            {item.location}
-                          </div>
-
-                          <div class="mt-2 flex items-center text-sm leading-5 text-gray-600 sm:mr-6 hh-job-row-experience">
-                            <inline-svg>
-                              <svg
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                                class="flex-shrink-0 mr-1 h-5 w-5 text-gray-500"
-                              >
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                ></path>
-                              </svg>
-                            </inline-svg>
-                            {item.time}
-                          </div>
-                        </div>
-                      </a>
-                    </div>
-                  </>
-                );
-              })}
-            </div>
-          </div>
-          <div class="bg-white p-5 sm:py-10 sm:px-0 hh-text-section border-t-2">
-            <div class="ql-editor max-w-screen-md mx-auto">
-              <p class="ql-align-justify">
-                <span style={{ color: "rgb(113, 111, 114)" }}>
-                  თუ ამჟამად გამოცხადებული ვაკანსიების ჩამონათვალში ვერ
-                  აღმოაჩინეთ თქვენთვის საინტერესო პოზიცია, შეგიძლიათ სამომავლოდ
-                  განსახილველად გამოგვიგზავნოთ თქვენი რეზიუმე{" "}
-                </span>
-                <a
-                  href="https://my.hirehive.io/jsc-bank-of-georgia/jobs/35823/"
-                  target="_blank"
-                >
-                  ბმულზე.
-                </a>
-                <span style={{ color: "rgb(113, 111, 114)" }}>
-                  {" "}
-                  მიღებული ინფორმაცია აისახება საქართველოს ბანკის აპლიკანტების
-                  ბაზაში და ტალანტების მოზიდვის გუნდი დაგიკავშირდებათ თქვენი
-                  ინტერესების, ცოდნისა და გამოცდილების შესაბამისი ვაკანსიის
-                  არსებობის შემთხვევაში.
-                </span>
-              </p>
-            </div>
-          </div>
-          <Notification />
-        </section>
-
-        <Footer />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" exact component={IndexPages} />
+          </Switch>
+        </BrowserRouter>
       </ApplicationContext.Provider>
     </>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
           <div className="flex flex-1">
             <img
               className="hh-company-logo max-h-16"
-              src='https://zartis.blob.core.windows.net/user/Company_8148/logo_1.png?sv=2019-07-07&amp;sr=b&amp;sig=ZTOnQZ9XKSGrhQUxlIcNztqaXP5QxE18vb%2BeyDLhkQ8%3D&amp;se=2021-09-12T20%3A33%3A59Z&amp;sp=r&amp;rscd=inline%3Bfilename%3D"logo_1.png"'
+              src="https://zartis.blob.core.windows.net/user/Company_8148/logo_1.png?sv=2019-07-07&sr=b&sig=mGv%2FlgJJrKhug2xgwgKrOx6Kc7YTdsgXiypmIrBC2%2FA%3D&se=2021-09-13T11%3A41%3A55Z&sp=r&rscd=inline%3Bfilename%3D%22logo_1.png%22"
               alt="JSC Bank of Georgia Logo"
             />
           </div>
