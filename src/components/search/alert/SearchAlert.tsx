331f6c35d764d76a234c8781654f1e5309450473
asyncfinkd <shamgoingzuck@gmail.com>
2021-09-17 10:17:15 +0400

    Optimization Search Alert

---
+++ src/components/search/alert/SearchAlert.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const SearchAlert: React.FC<any> = ({ setData, jobs, setSearchValue }: any) => {
+  return (
+    <>
+      <div className="fade-in animate-slow max-w-screen-md mx-auto mt-3">
+        <div
+          className="hh-no-search-results bg-white rounded p-4 flex items-center justify-center"
+          style={{ fontFamily: "MarkGEO" }}
+        >
+          <h3 className="font-semibold text-xl">
+            თქვენს მიერ მითითებული მონაცემებით არ იძებენება მიმდინარე ვაკანსია
+          </h3>
+          <a
+            className="ml-4 px-4 py-1 font-medium text-blue-500 text-sm rounded shadow-sm button border-blue-500 border-2 hover:text-white hover:bg-blue-500"
+            href="#jobs"
+            onClick={() => {
+              setData(jobs);
+              setSearchValue("");
+            }}
+          >
+            ნახე ყველა სამუშაო
+          </a>
+        </div>
+
+        <div className="hh-jobs-openings"></div>
+      </div>
+    </>
+  );
+};
+
+export default SearchAlert;
--- src/pages/index/indexPages.tsx
+++ src/pages/index/indexPages.tsx
@@ -10,6 +10,7 @@ import { Helmet } from "react-helmet";
 import IndexList from "../../components/index/components/IndexList";
 import IndexFooter from "../../components/index/components/IndexFooter";
 import { isArrayEmpty } from "../../utils";
+import SearchAlert from "../../components/search/alert/SearchAlert";
 
 const IndexPages: React.FC = () => {
   const { jobs } = React.useContext(ApplicationContext);
@@ -79,29 +80,11 @@ const IndexPages: React.FC = () => {
             })}
             {data.length == 0 && (
               <>
-                <div className="fade-in animate-slow max-w-screen-md mx-auto mt-3">
-                  <div
-                    className="hh-no-search-results bg-white rounded p-4 flex items-center justify-center"
-                    style={{ fontFamily: "MarkGEO" }}
-                  >
-                    <h3 className="font-semibold text-xl">
-                      თქვენს მიერ მითითებული მონაცემებით არ იძებენება მიმდინარე
-                      ვაკანსია
-                    </h3>
-                    <a
-                      className="ml-4 px-4 py-1 font-medium text-blue-500 text-sm rounded shadow-sm button border-blue-500 border-2 hover:text-white hover:bg-blue-500"
-                      href="#jobs"
-                      onClick={() => {
-                        setData(jobs);
-                        setSearchValue("");
-                      }}
-                    >
-                      ნახე ყველა სამუშაო
-                    </a>
-                  </div>
-
-                  <div className="hh-jobs-openings"></div>
-                </div>
+                <SearchAlert
+                  setSearchValue={setSearchValue}
+                  jobs={jobs}
+                  setData={setData}
+                />
               </>
             )}
           </div>
