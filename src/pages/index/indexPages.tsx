3bd9c4ec937976051fe864ef604834f829554035
asyncfinkd <shamgoingzuck@gmail.com>
2021-09-17 10:08:51 +0400

    Update indexPages.tsx

--- src/pages/index/indexPages.tsx
+++ src/pages/index/indexPages.tsx
@@ -79,33 +79,28 @@ const IndexPages: React.FC = () => {
             })}
             {data.length == 0 && (
               <>
-                <div
-                  id="jobs"
-                  className="bg-gray-200 border-t-2 p-4 md:py-10 md:px-0 hh-job-listings"
-                >
-                  <div className="fade-in animate-slow max-w-screen-md mx-auto">
-                    <div
-                      className="hh-no-search-results bg-white rounded p-4 flex items-center justify-center"
-                      style={{ fontFamily: "MarkGEO" }}
+                <div className="fade-in animate-slow max-w-screen-md mx-auto mt-3">
+                  <div
+                    className="hh-no-search-results bg-white rounded p-4 flex items-center justify-center"
+                    style={{ fontFamily: "MarkGEO" }}
+                  >
+                    <h3 className="font-semibold text-xl">
+                      თქვენს მიერ მითითებული მონაცემებით არ იძებენება მიმდინარე
+                      ვაკანსია
+                    </h3>
+                    <a
+                      className="ml-4 px-4 py-1 font-medium text-blue-500 text-sm rounded shadow-sm button border-blue-500 border-2 hover:text-white hover:bg-blue-500"
+                      href="#jobs"
+                      onClick={() => {
+                        setData(jobs);
+                        setSearchValue("");
+                      }}
                     >
-                      <h3 className="font-semibold text-xl">
-                        თქვენს მიერ მითითებული მონაცემებით არ იძებენება
-                        მიმდინარე ვაკანსია
-                      </h3>
-                      <a
-                        className="ml-4 px-4 py-1 font-medium text-blue-500 text-sm rounded shadow-sm button border-blue-500 border-2 hover:text-white hover:bg-blue-500"
-                        href="#jobs"
-                        onClick={() => {
-                          setData(jobs);
-                          setSearchValue("");
-                        }}
-                      >
-                        ნახე ყველა სამუშაო
-                      </a>
-                    </div>
-
-                    <div className="hh-jobs-openings"></div>
+                      ნახე ყველა სამუშაო
+                    </a>
                   </div>
+
+                  <div className="hh-jobs-openings"></div>
                 </div>
               </>
             )}
