eb14e8ad3135f14d2a6e703696bfc4ac0b4b071c
asyncfinkd <shamgoingzuck@gmail.com>
2021-11-20 10:59:43 +0400

    fix problems

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    '@typescript-eslint/no-var-requires': 0,
   },
 };
