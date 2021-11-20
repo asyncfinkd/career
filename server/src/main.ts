22de4a9f4869b186c2540428614591bc64ef7592
asyncfinkd <shamgoingzuck@gmail.com>
2021-11-20 10:56:44 +0400

    :whale2:  new features for development

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as compression from "compression";
-import * as express from "express";
-import * as helmet from "helmet";
+import * as compression from 'compression';
+import * as express from 'express';
+import * as helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  app.use(compression());
+  // app.use(helmet());
+
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
   await app.listen(process.env.PORT, () => {
     console.log(`server is already started ${process.env.PORT}`);
   });
