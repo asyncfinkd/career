257571e4940172dd04f59770f6eeb7d4c15f49ad
asyncfinkd <shamgoingzuck@gmail.com>
2021-11-21 13:44:09 +0400

    :whale2: notification and footer

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next';
+import Footer from 'ui/footer';
 import Header from 'ui/header';
 import Hero from 'ui/hero';
+import Notification from 'ui/notification';
 
 const Home: NextPage = () => {
   return (
     <>
       <Header />
       <Hero />
+      <Notification />
+      <Footer />
     </>
   );
 };
---
+++ client/ui/notification/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+export default function Notification() {
+  return (
+    <>
+      <div className="bg-white p-5 sm:py-10 sm:px-0 hh-text-section border-t-2">
+        <div className="ql-editor max-w-screen-md mx-auto">
+          <p className="ql-align-justify">
+            <span
+              style={{ color: 'rgb(113, 111, 114)', fontFamily: 'MarkGEO' }}
+            >
+              თუ ამჟამად გამოცხადებული ვაკანსიების ჩამონათვალში ვერ აღმოაჩინეთ
+              თქვენთვის საინტერესო პოზიცია, შეგიძლიათ სამომავლოდ განსახილველად
+              გამოგვიგზავნოთ თქვენი რეზიუმე{' '}
+            </span>
+            <a
+              href="https://my.hirehive.io/jsc-bank-of-georgia/jobs/35823/"
+              target="_blank"
+              style={{ fontFamily: 'MarkGEO' }}
+            >
+              ბმულზე.
+            </a>
+            <span
+              style={{ color: 'rgb(113, 111, 114)', fontFamily: 'MarkGEO' }}
+            >
+              {' '}
+              მიღებული ინფორმაცია აისახება საქართველოს ბანკის აპლიკანტების
+              ბაზაში და ტალანტების მოზიდვის გუნდი დაგიკავშირდებათ თქვენი
+              ინტერესების, ცოდნისა და გამოცდილების შესაბამისი ვაკანსიის
+              არსებობის შემთხვევაში.
+            </span>
+          </p>
+        </div>
+      </div>
+      <div className="bg-white p-5 sm:py-10 sm:px-0 hh-text-section border-t-2">
+        <div className="ql-editor max-w-screen-md mx-auto">
+          <h2>
+            <br />
+          </h2>
+          <p className="ql-align-center">
+            <span style={{ color: 'rgb(91,91,91)', fontFamily: 'MarkGEO' }}>
+              ტალანტების მოზიდვის გუნდი&nbsp;
+            </span>
+          </p>
+          <p className="ql-align-center">
+            <span style={{ color: 'rgb(91,91,91)', fontFamily: 'MarkGEO' }}>
+              საქართველოს ბანკი
+            </span>
+          </p>
+          <p className="ql-align-center">
+            <span style={{ color: 'rgb(91,91,91)', fontFamily: 'MarkGEO' }}>
+              ელ.ფოსტა: Careers@bog.ge&nbsp;
+            </span>
+          </p>
+          <p className="ql-align-center">
+            <span style={{ color: 'rgb(91,91,91)', fontFamily: 'MarkGEO' }}>
+              ტელეფონი: (+995 32) 2444444 (ext. 4422)
+            </span>
+            .
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
