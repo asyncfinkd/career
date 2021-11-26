dfe1ebc3bfc162a05eb4f79cd4f1b3627d83008a
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-26 21:48:17 +0400

    :whale2: more identification

--- client/pages/admin/index.tsx
+++ client/pages/admin/index.tsx
@@ -1,10 +1,31 @@
 import AdminModules from 'modules/admin';
 import React from 'react';
+import type { GetServerSideProps, GetServerSidePropsContext } from 'next';
 
-export default function AdminPages() {
+function AdminPages() {
   return (
     <>
       <AdminModules />
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext,
+) => {
+  const { req } = context;
+  const { cookies } = req;
+
+  if (cookies.cookie) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    };
+  }
+
+  return { props: { success: true } };
+};
+
+export default AdminPages;
