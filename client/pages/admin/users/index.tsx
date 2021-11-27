784cfbc2b6a0a0893e938bae713a87385e13e4a3
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-27 09:58:10 +0400

    :whale2: start building admin users pages.

---
+++ client/pages/admin/users/index.tsx
@@ -0,0 +1,10 @@
+import AdminUsersModules from 'modules/admin/users';
+import React from 'react';
+
+export default function AdminUsersPages() {
+  return (
+    <>
+      <AdminUsersModules />
+    </>
+  );
+}
