36f724fc4eb783820ebfedb31d595391ac752743
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-28 10:47:22 +0400

    add button and form for edit modal

--- client/modules/admin/users/components/users-edit-modal.tsx
+++ client/modules/admin/users/components/users-edit-modal.tsx
@@ -13,6 +13,7 @@ import {
   SelectChangeEvent,
 } from '@mui/material';
 import { UsersForm } from 'fixtures/modules/admin/users';
+import Button from 'components/button';
 
 const style = {
   position: 'absolute' as 'absolute',
@@ -36,51 +37,56 @@ export default function UsersEditModal({ open, handleClose, item }: any) {
 
   return (
     <>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography
-            sx={{ fontFamily: 'MarkGEO' }}
-            id="modal-modal-title"
-            variant="h6"
-            component="h2"
-          >
-            ინფორმაციის შეცვლა
-          </Typography>
-          <FormControl fullWidth sx={{ marginTop: '20px' }}>
-            <InputLabel id="demo-simple-select-label">მომხმარებელი</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={age}
-              label="მომხმარებელი"
-              onChange={handleChange}
+      <form>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography
+              sx={{ fontFamily: 'MarkGEO' }}
+              id="modal-modal-title"
+              variant="h6"
+              component="h2"
             >
-              {data.map((item: any) => {
-                return <MenuItem value={item._id}>{item.fullName}</MenuItem>;
-              })}
-            </Select>
-          </FormControl>
-          {age != '' && (
-            <>
-              {UsersForm.map((secItem: any) => {
-                return (
-                  <TextField
-                    id="outlined-basic"
-                    sx={{ marginTop: '20px', width: '100%' }}
-                    label={secItem.text}
-                    variant="outlined"
-                  />
-                );
-              })}
-            </>
-          )}
-        </Box>
-      </Modal>
+              ინფორმაციის შეცვლა
+            </Typography>
+            <FormControl fullWidth sx={{ marginTop: '20px' }}>
+              <InputLabel id="demo-simple-select-label">
+                მომხმარებელი
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="მომხმარებელი"
+                onChange={handleChange}
+              >
+                {data.map((item: any) => {
+                  return <MenuItem value={item._id}>{item.fullName}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+            {age != '' && (
+              <>
+                {UsersForm.map((secItem: any) => {
+                  return (
+                    <TextField
+                      id="outlined-basic"
+                      sx={{ marginTop: '20px', width: '100%' }}
+                      label={secItem.text}
+                      variant="outlined"
+                    />
+                  );
+                })}
+                <Button>დამატება</Button>
+              </>
+            )}
+          </Box>
+        </Modal>
+      </form>
     </>
   );
 }
