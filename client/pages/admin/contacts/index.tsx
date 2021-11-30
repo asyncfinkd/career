781167e9f9db6fe5ad2458db7400985475c955f5
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-11-30 15:30:09 +0400

    add ssr

--- client/pages/admin/contacts/index.tsx
+++ client/pages/admin/contacts/index.tsx
@@ -1,4 +1,5 @@
 import AdminContactsModules from 'modules/admin/contacts';
+import { GetServerSidePropsContext } from 'next';
 import React from 'react';
 
 function AdminContactsPages() {
@@ -7,6 +8,35 @@ function AdminContactsPages() {
       <AdminContactsModules />
     </>
   );
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { req } = context;
+  const { cookies } = req;
+
+  const request = await fetch(
+    `${process.env.SERVER_API_URL}/api/check/logged`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${cookies.cookie}`,
+      },
+    },
+  );
+
+  const response = await request.json();
+
+  if (response.statusCode == 401) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/',
+      },
+    };
+  }
+
+  return { props: { data: response.user } };
 }
 
 export default AdminContactsPages;
