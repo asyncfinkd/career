38cbdfa14949322ec9c7e759b10c6aab9ca37fae
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-12-01 18:22:05 +0400

    by dynamically and add box for gaping items flexbox.

--- client/modules/admin/contacts/components/contacts-hero.tsx
+++ client/modules/admin/contacts/components/contacts-hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Chip from '@mui/material/Chip';
 import { TAdminContactsInterface } from 'types/pages/admin/contacts';
+import { Box } from '@mui/system';
 
 export default function ContactsHero({
   contactsItem,
@@ -9,11 +10,18 @@ export default function ContactsHero({
 }) {
   return (
     <>
-      <Chip
-        label="Chip Outlined"
-        variant="outlined"
-        sx={{ cursor: 'pointer' }}
-      />
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+        <Chip
+          label={contactsItem.email}
+          variant="outlined"
+          sx={{ cursor: 'pointer' }}
+        />
+        <Chip
+          label={contactsItem.phone}
+          variant="outlined"
+          sx={{ cursor: 'pointer' }}
+        />
+      </Box>
     </>
   );
 }
