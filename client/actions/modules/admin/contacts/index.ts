ad23f6aeade4f83f7989991f2e296a42f30d0b80
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-12-01 12:16:18 +0400

    :rocket: contact page

---
+++ client/actions/modules/admin/contacts/index.ts
@@ -0,0 +1,23 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AdminSidebar from 'ui/admin/sidebar';
+import Head from 'next/head';
+
+export default {
+  Head,
+  AdminSidebar,
+  Typography,
+  Toolbar,
+  MenuIcon,
+  IconButton,
+  Drawer,
+  CssBaseline,
+  Box,
+  AppBar,
+};
